bba522e5eda73f2c27fb03c0b30705567f93d91c
Alek Novkovski <alek@greatcode2.com>
2023-01-25 00:13:54 +0100

    Move password hashing into mongoose middleware

--- working/controllers/auth.js
+++ working/controllers/auth.js
@@ -1,15 +1,8 @@
 const { StatusCodes } = require('http-status-codes')
 const User = require('../models/User')
-const bcrypt = require('bcryptjs')
 
 async function register(req, res) {
-    const {name, email, password} = req.body
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt)
-    const tempUser = {name, email, password: hashedPassword}
-
-    const user = await User.create({...tempUser})
+    const user = await User.create({...req.body})
     res.status(StatusCodes.CREATED).json({message: 'Register route', submitted: req.body, user} )
 }
 
