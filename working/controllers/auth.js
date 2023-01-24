766eb2dca37ede2fa80ca750a6f1c494213673c0
Alek Novkovski <alek@greatcode2.com>
2023-01-24 23:39:44 +0100

    Hash password before storing in DB

--- working/controllers/auth.js
+++ working/controllers/auth.js
@@ -1,7 +1,16 @@
 const { StatusCodes } = require('http-status-codes')
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
 
 async function register(req, res) {
-    res.status(StatusCodes.CREATED).json({message: 'Register route', submitted: req.body} )
+    const {name, email, password} = req.body
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const tempUser = {name, email, password: hashedPassword}
+
+    const user = await User.create({...tempUser})
+    res.status(StatusCodes.CREATED).json({message: 'Register route', submitted: req.body, user} )
 }
 
 async function login(req, res) {
