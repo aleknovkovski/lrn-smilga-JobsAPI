efee9ce844c94e47d5b0938a40a220b04825169d
Alek Novkovski <alek@greatcode2.com>
2023-01-25 13:16:48 +0100

    Generate JWT for new users, send back on register

--- working/controllers/auth.js
+++ working/controllers/auth.js
@@ -1,9 +1,11 @@
 const { StatusCodes } = require('http-status-codes')
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 async function register(req, res) {
     const user = await User.create({...req.body})
-    res.status(StatusCodes.CREATED).json({message: 'Register route', submitted: req.body, user} )
+    const token = jwt.sign({userId: user._id.toString(), name: user.name}, 'jwtSecret')
+    res.status(StatusCodes.CREATED).json({message: 'Register route', submitted: req.body, user, user: { name: user.name }, token } )
 }
 
 async function login(req, res) {
