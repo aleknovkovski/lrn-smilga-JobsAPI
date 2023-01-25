fa00df7c0b7943577d6a3fe548c705a1e872ec02
Alek Novkovski <alek@greatcode2.com>
2023-01-25 18:40:40 +0100

    Move JWT generation into model instance method

--- working/controllers/auth.js
+++ working/controllers/auth.js
@@ -5,7 +5,11 @@ const jwt = require('jsonwebtoken')
 async function register(req, res) {
     const user = await User.create({...req.body})
     const token = jwt.sign({userId: user._id.toString(), name: user.name}, 'jwtSecret')
-    res.status(StatusCodes.CREATED).json({message: 'Register route', submitted: req.body, user, user: { name: user.name }, token } )
+    res.status(StatusCodes.CREATED).json({
+        message: 'Register route',
+        submitted: req.body, user,
+        user: { name: user.name },
+        token: user.getJWTToken() } )
 }
 
 async function login(req, res) {
