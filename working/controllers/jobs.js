b5650bb3a46552a3e3a34cd272515da3305990b7
Alek Novkovski <alek@greatcode2.com>
2023-01-26 13:34:28 +0100

    Build updateJob controller

--- working/controllers/jobs.js
+++ working/controllers/jobs.js
@@ -35,8 +35,25 @@ async function createJob (req, res) {
     });
 }
 
-async function updateJob (req, res) {
-    res.send('Update a job');
+const updateJob = async (req, res) => {
+    const {
+        body: { company, position },
+        user: { userId },
+        params: { id: jobId },
+    } = req
+
+    if (company === '' || position === '') {
+        throw new BadRequestError('Company or Position fields cannot be empty')
+    }
+    const job = await Job.findByIdAndUpdate(
+        { _id: jobId, createdBy: userId },
+        {company, position},
+        { new: true, runValidators: true }
+    )
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 
 async function deleteJob (req, res) {
